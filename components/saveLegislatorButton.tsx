26ed12136cd96eb007f6a471693df402e6025d86
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-05-31 11:27:29 -0700

    save legislator button is disabled instantly when you save a legislator. save legislator is disabled on mount if legislator is in fetched list of your saved legislators

--- components/saveLegislatorButton.tsx
+++ components/saveLegislatorButton.tsx
@@ -1,6 +1,34 @@
 'use client'
 
+import { useState, useEffect } from 'react';
+
 export default function SaveLegislator({ bioguideId, name }) {
+    const [records, setRecords] = useState([])
+    const [legislatorSaved, setLegislatorSaved] = useState(false)
+
+    useEffect(() => {
+        const fetchRecords = async () => {
+            try {
+                const response = await fetch(`/api/view-records`);
+                if (!response.ok) {
+                    throw new Error(`http error: ${response.status}`)
+                }
+                const data = await response.json();
+                console.log("my important data", data)
+                setRecords(data);
+                for (let legislator of data) {
+                    if (legislator.info == `${bioguideId}`) {
+                        setLegislatorSaved(true);
+                    }
+                }
+
+            }
+            catch (error) {
+                console.error(`failed to fetch records: ${error}`)
+            }
+        }
+        fetchRecords()
+    }, [])
 
     const handleClick = async () => {
         try {
@@ -19,6 +47,7 @@ export default function SaveLegislator({ bioguideId, name }) {
             }
             const data = await response.json();
             console.log(data);
+            setLegislatorSaved(true);
         }
         catch (error) {
             console.error(`failed to save bill: ${error}`)
@@ -26,6 +55,6 @@ export default function SaveLegislator({ bioguideId, name }) {
     }
 
     return (
-        <button onClick={handleClick} className="mb-5 bg-indigo-500 hover:bg-indigo-600 text-white font-bold px-4 py-2 rounded" type="submit">Save Legislator</button>
+        <button onClick={handleClick} className="mb-5 bg-indigo-500 hover:bg-indigo-600 text-white font-bold px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed" disabled={legislatorSaved} type="submit">Save Legislator</button>
     )
 }
