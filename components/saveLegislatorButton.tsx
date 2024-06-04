809ffe9bcdf8f2a16fd61872e8c1bc83e81a388b
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-06-04 10:43:57 -0700

    Un-authenticated user gets a warning message when they try to save a legislator. Message is based on the response status that comes back from the backend

--- components/saveLegislatorButton.tsx
+++ components/saveLegislatorButton.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react';
 export default function SaveLegislator({ bioguideId, name }) {
     const [records, setRecords] = useState([])
     const [legislatorSaved, setLegislatorSaved] = useState(false)
+    const [responseStatus, setResponseStatus] = useState("200");
+
 
     useEffect(() => {
         const fetchRecords = async () => {
@@ -42,6 +44,7 @@ export default function SaveLegislator({ bioguideId, name }) {
             }
             console.log(requestOptions)
             const response = await fetch(`/api/add-record`, requestOptions);
+            setResponseStatus(response.status)
             if (!response.ok) {
                 throw new Error(`http error: ${response.status}`)
             }
@@ -55,6 +58,11 @@ export default function SaveLegislator({ bioguideId, name }) {
     }
 
     return (
-        <button onClick={handleClick} className="mb-5 bg-indigo-500 hover:bg-indigo-600 text-white font-bold px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed" disabled={legislatorSaved} type="submit">Save Legislator</button>
+        <>
+            <button onClick={handleClick} className="mb-4 bg-indigo-500 hover:bg-indigo-600 text-white font-bold px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed" disabled={legislatorSaved} type="submit">Save Legislator</button>
+            {responseStatus.toString().includes("4") || responseStatus.toString().includes("5") && (
+                <div className="text-emerald-600 bg-emerald-100 p-4 rounded mb-5">Unable to save legislator to your saved items. Please make sure you're signed up and logged in!</div>
+            )}
+        </>
     )
 }
