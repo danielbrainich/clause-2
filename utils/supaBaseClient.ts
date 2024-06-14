60d0668f7a59804fd17e71d3cdd00337bcf23e2c
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-06-14 13:57:07 -0700

    addressed some linting warning that were blocking deploy

--- utils/supaBaseClient.ts
+++ utils/supaBaseClient.ts
@@ -1,6 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
-const supabaseKey = process.env.SUPABASE_API_KEY;
+const supabaseUrl = process.env.SUPABASE_PROJECT_URL as string;
+const supabaseKey = process.env.SUPABASE_API_KEY as string;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and API key must be provided");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
